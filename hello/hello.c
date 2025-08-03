f4de6e2471d64079266a48f37b99d12e1ea153c2
Fun Kacer <funkacer@gmail.com>
2025-08-03 11:50:49 +0200

    basics

---
+++ hello/hello.c
@@ -0,0 +1,11 @@
+// code hello.c
+// install microsoft extension
+// make hello
+// ./hello
+
+#include <stdio.h>
+
+int main(void)
+{
+    printf("hello, \"world\"!\n");
+}
---
+++ input/input.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+
+int main(void)
+{
+
+    /* 
+    // Create a string
+    char firstName[30];
+
+    // Ask the user to input some text
+    printf("Enter your first name: \n");
+
+    // Get and save the text
+    scanf("%s", firstName);
+
+    // Output the text
+    printf("Hello %s\n", firstName); */
+
+    char fullName[30];
+
+    printf("Type your full name: \n");
+    fgets(fullName, sizeof(fullName), stdin);
+
+    printf("Hello %s", fullName);
+
+    // Type your full name: John Doe
+    // Hello John Doe
+
+    // fullName << cin;
+
+    // Create an int and a char variable
+    int myNum;
+    char myChar;
+
+    // Ask the user to type a number AND a character
+    printf("Type a number AND a character and press enter: \n");
+
+    // Get and save the number AND character the user types
+    scanf("%d %c", &myNum, &myChar);
+
+    // Print the number
+    printf("Your number is: %d\n", myNum);
+
+    // Print the character
+    printf("Your character is: %c\n", myChar);
+    
+}
