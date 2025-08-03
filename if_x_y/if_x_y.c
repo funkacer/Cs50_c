c0c55163dde715a9482d08b500cb6844de561c8b
Fun Kacer <funkacer@gmail.com>
2025-08-03 20:09:23 +0200

    if x<y

---
+++ if_x_y/if_x_y.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main(void)
+{
+
+    // Create an int and a char variable
+    int x;
+    int y;
+
+    // Ask the user to type a number AND a character
+    printf("Type a number x AND a number y and press enter: \n");
+
+    // Get and save the number AND character the user types
+    scanf("%d %d", &x, &y);
+
+    // Print the number
+    printf("Your number x is: %d\n", x);
+
+    // Print the character
+    printf("Your number y is: %d\n", y);
+    
+    if (x < y)
+    {
+        printf("x is less than y\n");
+    } else
+    {
+        printf("x is NOT less than y\n");
+    }
+}
