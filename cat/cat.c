a5138eaa399065dfc227f0cf07a3304b812ceb3c
Jiří Černý <funkacer@gmail.com>
2025-08-09 18:30:34 +0200

    basic concepts in c

--- cat/cat.c
+++ cat/cat.c
@@ -1,16 +1,37 @@
 #include <stdio.h>
 
+void meow(int n);
+int get_positive_int(void);
+
 int main(void)
+{
+    int n = get_positive_int();
+    meow(n);
+}
+
+int get_positive_int(void)
+{
+    int n;
+    do
+    {
+        printf("Number: ");
+        scanf("%d", &n);
+    } while (n < 1);
+
+    return n;
+}
+
+void meow(int n)
 {
     int i = 0;
 
-    while (i < 3)
+    while (i < n)
     {
         printf("meow 1:%d\n", i);
         i++;
     }
 
-    for (int i = 0; i < 3; i++)
+    for (int i = 0; i < n; i++)
     {
         printf("meow 2:%d\n", i);
     }
