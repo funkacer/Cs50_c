9cf54849f140e25450e34631c1b3f0cb0a3d17c8
Jiří Černý <funkacer@gmail.com>
2025-10-08 18:49:55 +0200

    selection sort (moje verze)

---
+++ selection_sort/selection_sort.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int numbers[] = {7, 2, 5, 4, 1, 6, 0, 3};
+    //printf("%lu\n", sizeof(numbers));
+    for (int i = 0; i < sizeof(numbers)/4; i++)
+    {
+        int min = i;
+        for (int j = i + 1; j < sizeof(numbers)/4; j++)
+        {
+            if (numbers[j] < numbers[min])
+            {
+                min = numbers[j];
+                min = j;
+            }
+        }
+        if (min > i)
+        {
+            int temp = numbers[i];
+            numbers[i] = numbers[min];
+            numbers[min] = temp;
+        }
+    }
+    for (int i = 0; i < sizeof(numbers)/4; i++)
+    {
+        printf("%d\n", numbers[i]);
+    }
+}
